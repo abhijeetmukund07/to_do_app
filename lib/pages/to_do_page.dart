561a2127fcd73bb43800cc4078b58479ef0a0f33
abhijeetmukund07 <abhijeetmukund@gmail.com>
2024-06-09 16:44:30 +0530

    1. Fixed the textFieldto appear on top of keyboard 2. added dismissable widget to toDoTile()

--- lib/pages/to_do_page.dart
+++ lib/pages/to_do_page.dart
@@ -17,32 +17,67 @@ class _ToDoPageState extends State<ToDoPage> {
     ['Build a ToDo App', false],
   ];
 
+  int completedTasksCount = 0;
+
   void toggleTaskStatus(bool? value, int index) {
     setState(() {
       tasks[index][1] = !tasks[index][1];
+      if (value == true) {
+        completedTasksCount++;
+      } else {
+        completedTasksCount--; //*** else accounts for false when clicked on checkbox or slid left to right which gives value as null
+      }
     });
   }
 
-  void addNewTask() {
-    tasks.add(['Learn Flutter', false]);
+  void _addNewTask() {
+    setState(() {
+      tasks.add(['Learn Flutter', false]);
+    });
   }
 
-  void createNewTask() {
+  void deleteTask(int index) {
+    setState(() {
+      tasks.removeAt(index);
+      completedTasksCount--; // ***** Might need a review ****
+    });
+  }
+
+  void showAddNewTaskTextField() {
     showModalBottomSheet(
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(0))),
       context: context,
-      isScrollControlled: true, // This makes sure the sheet is resized when the keyboard is shown
+      isScrollControlled:
+          true, // This makes sure the sheet is resized when the keyboard is shown
       builder: (context) {
-        return Padding(
-            padding: EdgeInsets.only(
-              bottom: MediaQuery.of(context)
-                  .viewInsets
-                  .bottom, // This ensures the bottom padding is enough for the keyboard
+        return Container(
+          height: 45,
+          margin:
+              EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+          child: Expanded(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(15, 7, 15, 15),
+              child: TextField(
+                autofocus: true,
+                decoration: InputDecoration(
+                    suffixIcon: IconButton(
+                        onPressed: _addNewTask,
+                        icon: const Padding(
+                          padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
+                          child: Icon(
+                            Icons.add,
+                            // weight: 15,
+                            size: 25,
+                          ),
+                        )),
+                    fillColor: Colors.amberAccent,
+                    border: InputBorder.none),
+                style: const TextStyle(fontSize: 18),
+              ),
             ),
-            child: TextField(
-              decoration: InputDecoration(
-                  suffixIcon: IconButton(
-                      onPressed: addNewTask, icon: const Icon(Icons.add))),
-            ));
+          ),
+        );
       },
     );
   }
@@ -63,14 +98,35 @@ class _ToDoPageState extends State<ToDoPage> {
           child: ListView.builder(
         itemCount: tasks.length,
         itemBuilder: (context, index) {
-          return ToDoTile(
-              taskName: tasks[index][0],
-              taskStatus: tasks[index][1],
-              onChanged: (value) => toggleTaskStatus(value, index));
+          return Dismissible(
+            key: UniqueKey(),
+            background: Container(
+              color: Colors.green,
+              alignment: Alignment.centerLeft,
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: const Icon(Icons.check, color: Colors.white),
+            ),
+            secondaryBackground: Container(
+              color: Colors.red,
+              alignment: Alignment.centerRight,
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: const Icon(Icons.delete, color: Colors.white),
+            ),
+            onDismissed: (direction) => {
+              if (direction == DismissDirection.startToEnd)
+                {toggleTaskStatus(null, index)}
+              else if (direction == DismissDirection.endToStart)
+                {deleteTask(index)}
+            },
+            child: ToDoTile(
+                taskName: tasks[index][0],
+                taskStatus: tasks[index][1],
+                onChanged: (value) => toggleTaskStatus(value, index)),
+          );
         },
       )),
       floatingActionButton: FloatingActionButton(
-        onPressed: createNewTask,
+        onPressed: showAddNewTaskTextField,
         child: const Icon(Icons.add),
       ),
     );
